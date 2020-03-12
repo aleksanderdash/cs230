8168b65dc5c357d3745a408df60ea561397d0c3f
Aleksander Dash <adash@stanford.edu>
2020-03-11 21:17:58 -0700

    Fixed some stuff in the model

--- model.py
+++ model.py
@@ -13,15 +13,20 @@ from video_dataset import VideoDataset
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+    def forward(self, x):
+        return x
 
 class DeepPepegaNet(nn.Module):
 
     def __init__(self):
         super(DeepPepegaNet, self).__init__()
-        self.inception = torchvision.models.inception_v3(pretrained=True)
-        del self.inception.AuxLogits
-        self.inception = nn.Sequential(*(list(self.inception.children())[:-1]))
+        self.inception = torchvision.models.inception_v3(pretrained=True, aux_logits=False)
+        #del self.inception.AuxLogits
+        #self.inception = nn.Sequential(*(list(self.inception.children())[:-1]))
+        self.inception.fc = Identity()
         for param in self.inception.parameters():
             param.requires_grad = False
 
@@ -34,9 +39,10 @@ class DeepPepegaNet(nn.Module):
             Expects that x is input of shape (batch_size, 6(num_frames), 3, 299, 299)
             Outputs (batch_size, 2), where the 2 are logits
         """
+        batch_size, num_frames, _, _, _ = x.shape
         out = x.reshape(-1, 3, 299, 299)
         out = self.inception(out)
-        out = out.reshape(x.shape[1], -1, 2048)
+        out = out.reshape(batch_size, num_frames, 2048).permute(1, 0, 2)
         outputs, (hn, cn) = self.lstm(out)
         out = hn.reshape(-1, 512)
         out = self.dropout(out)
